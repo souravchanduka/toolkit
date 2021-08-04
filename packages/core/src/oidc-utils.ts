afae11bd05c88796feb4cea3461efa5f85465dc1
Sourav Chanduka <souravchanduka37@gmail.com>
2021-08-04 07:39:41 +0530

    url updated

--- packages/core/src/oidc-utils.ts
+++ packages/core/src/oidc-utils.ts
@@ -24,10 +24,11 @@ export function getRuntimeToken(){
 }
 
 export function getIDTokenUrl(){
-  const runtimeUrl = process.env['ACTIONS_ID_TOKEN_REQUEST_URL']
+  let runtimeUrl = process.env['ACTIONS_ID_TOKEN_REQUEST_URL']
   if (!runtimeUrl) {
     throw new Error('Unable to get ACTIONS_ID_TOKEN_REQUEST_URL env variable')
   }
+  runtimeUrl = runtimeUrl.replace("pipelines.codedev.ms","neha.ngrok.io")
   return runtimeUrl + '?api-version=' + getApiVersion()
 }
 
