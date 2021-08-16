8d2b375b7f15a9ac7f1da071a7e5a0143d74d612
Sourav Chanduka <souravchanduka37@gmail.com>
2021-08-16 13:30:22 +0530

    payload updated

--- packages/core/src/oidc-utils.ts
+++ packages/core/src/oidc-utils.ts
@@ -44,7 +44,7 @@ export class OidcClient {
   }
 
   private static async postCall(httpclient: actions_http_client.HttpClient, id_token_url: string, audience: string): Promise<string> {
-    const data :TokenRequest = { aud: !!audience ? audience : undefined}
+    const data :TokenRequest = { aud: !!audience ? audience : ''}
 
     debug(`audience is ${audience !== null ? audience : 'null'}`)
 
